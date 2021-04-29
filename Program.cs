30bfb3de6d8c4843ea41115c9336a13a21ec8dee
Attila772 <71554727+Attila772@users.noreply.github.com>
2021-04-29 15:20:54 +0200

    Commit test
    aaaaa

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace SzoftMod
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hell!");
         }
     }
 }

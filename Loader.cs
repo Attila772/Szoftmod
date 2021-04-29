a96069b7fcee0772b5e20a93e571fe9566113900
Attila772 <71554727+Attila772@users.noreply.github.com>
2021-04-29 18:21:41 +0200

    base

---
+++ Loader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace SzoftMod
+{
+    interface ILoader
+    {
+        public Subscribers loadSubscribers();
+    }
+
+   class Loader :ILoader
+    {
+        public Subscribers loadSubscribers()
+        {
+            Subscribers subscribers = JsonConvert.DeserializeObject<Subscribers>(File.ReadAllText(@"subscribers.json"));
+
+            foreach(Subscriber b in subscribers.subscribers) {
+                Console.WriteLine(b.airConditionerType);
+            }
+            return subscribers;
+        }
+        
+    }
+
+    public class Temperature
+    {
+        public string period { get; set; }
+        public double temperature { get; set; }
+    }
+    public class Subscriber
+    {
+        public string subscriber { get; set; }
+        public string homeId { get; set; }
+        public string boilerType { get; set; }
+        public string airConditionerType { get; set; }
+        public List<Temperature> temperatures { get; set; }
+    }
+    public class Subscribers
+    {
+        public List<Subscriber> subscribers { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
+
 namespace SzoftMod
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hell!");
+            Loader loader = new Loader();
+            loader.loadSubscribers();
         }
     }
+
+
+
+   
 }
